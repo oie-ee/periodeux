84ac02f8be769319d5f1832eb36eeb7ccbaf5c73
Sophie Kremer <sophieckremer@gmail.com>
2020-12-05 13:33:05 +0100

    Initial Commit

---
+++ periodeux/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  periodeux
+//
+//  Created by Sophie Kremer on 05.12.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ periodeux/periodeuxApp.swift
@@ -0,0 +1,17 @@
+//
+//  periodeuxApp.swift
+//  periodeux
+//
+//  Created by Sophie Kremer on 05.12.20.
+//
+
+import SwiftUI
+
+@main
+struct periodeuxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
