f0e7f9a68308383ce1cbb2b970e411777d457b35
Sophie Kremer <sophieckremer@gmail.com>
2021-01-17 16:42:35 +0100

    moved Weekdays() to an external view

--- periodeux/views/CalendarView.swift
+++ periodeux/views/CalendarView.swift
@@ -182,39 +182,6 @@ struct CalendarView: View {
         }
 }
 
-struct WeekDays: View {
-    let firstWeekDay = Calendar.current.firstWeekday
-    let weekdays = Calendar.current.shortWeekdaySymbols
-    
-    var weekdayRange: ArraySlice<String> {
-        print(firstWeekDay)
-        
-        guard firstWeekDay > 1 else {
-            return weekdays[0...weekdays.count - 1]
-        }
-        return weekdays[(firstWeekDay - 1)...(weekdays.count - 1)] + weekdays[0...(firstWeekDay - 2)]
-        
-    }
-    
-    var body: some View {
-        
-        HStack{
-            ForEach(weekdayRange, id: \.self) { weekdays in
-                
-                Spacer()
-                Text("\(weekdays)")
-                    .font(.subheadline)
-                    .fontWeight(.semibold)
-                    .foregroundColor(Color(.systemGray2))
-                    .textCase(.uppercase)
-                
-                Spacer()
-            }
-        }.padding([.leading, .trailing], 8)
-    }
-}
-
-
 struct CalendarView_Previews: PreviewProvider {
     static var previews: some View {
         CalendarView()
---
+++ periodeux/views/WeekdaysView.swift
@@ -0,0 +1,34 @@
+
+import SwiftUI
+
+struct WeekDays: View {
+    let firstWeekDay = Calendar.current.firstWeekday
+    let weekdays = Calendar.current.shortWeekdaySymbols
+    
+    var weekdayRange: ArraySlice<String> {
+        print(firstWeekDay)
+        
+        guard firstWeekDay > 1 else {
+            return weekdays[0...weekdays.count - 1]
+        }
+        return weekdays[(firstWeekDay - 1)...(weekdays.count - 1)] + weekdays[0...(firstWeekDay - 2)]
+        
+    }
+    
+    var body: some View {
+        
+        HStack{
+            ForEach(weekdayRange, id: \.self) { weekdays in
+                
+                Spacer()
+                Text("\(weekdays)")
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    .foregroundColor(Color(.systemGray2))
+                    .textCase(.uppercase)
+                
+                Spacer()
+            }
+        }.padding([.leading, .trailing], 8)
+    }
+}
