d4a84193d583c1f36784eccfc26e8ee1737c4e62
Sophie Kremer <sophieckremer@gmail.com>
2021-07-05 12:26:29 +0200

    changed spelling of assets for cellViews

--- periodeux/views/cellViews/SymptomModel.swift
+++ periodeux/views/cellViews/SymptomModel.swift
@@ -11,10 +11,10 @@ extension SymptomModel {
     
     static var symptom1  = SymptomModel(name: "All Is Well", imageIcon: "symptom:alliswell")
     static var symptom2  = SymptomModel(name: "Cramps", imageIcon: "symptom:cramps")
-    static var symptom3  = SymptomModel(name: "Sensitive Breast", imageIcon: "symptom:sensitivebreast")
+    static var symptom3  = SymptomModel(name: "Sensitive Breasts", imageIcon: "symptom:sensitivebreasts")
     static var symptom4  = SymptomModel(name: "Headache", imageIcon: "symptom:headache")
     static var symptom5  = SymptomModel(name: "Acne", imageIcon: "symptom:acne")
-    static var symptom6  = SymptomModel(name: "Lower Back Pain", imageIcon: "app.fill")
+    static var symptom6  = SymptomModel(name: "Lower Back Pain", imageIcon: "symptom:lowerbackpain")
     static var symptom7  = SymptomModel(name: "Nausea", imageIcon: "symptom:nausea")
     static var symptom8  = SymptomModel(name: "Tiredness", imageIcon: "symptom:tiredness")
     static var symptom9  = SymptomModel(name: "Flatulence", imageIcon: "symptom:flatulence")
@@ -22,7 +22,6 @@ extension SymptomModel {
     static var symptom11 = SymptomModel(name: "Insomnia", imageIcon: "symptom:insomnia")
     static var symptom12 = SymptomModel(name: "Constipa-\ntion", imageIcon: "symptom:constipa-tion")
     static var symptom13 = SymptomModel(name: "Diarrhea", imageIcon: "symptom:diarrhea")
-    static var symptom14 = SymptomModel(name: "Lower Back Pain", imageIcon: "symptom:lowerbackpain")
 }
 
-let symptomModel = [SymptomModel.symptom5, SymptomModel.symptom1, SymptomModel.symptom12, SymptomModel.symptom2, SymptomModel.symptom13, SymptomModel.symptom9, SymptomModel.symptom4, SymptomModel.symptom11, SymptomModel.symptom14, SymptomModel.symptom7, SymptomModel.symptom10, SymptomModel.symptom3, SymptomModel.symptom8]
+let symptomModel = [SymptomModel.symptom5, SymptomModel.symptom1, SymptomModel.symptom12, SymptomModel.symptom2, SymptomModel.symptom13, SymptomModel.symptom9, SymptomModel.symptom4, SymptomModel.symptom11, SymptomModel.symptom6, SymptomModel.symptom7, SymptomModel.symptom10, SymptomModel.symptom3, SymptomModel.symptom8]
